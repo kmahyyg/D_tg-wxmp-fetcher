8d918ead9164a7ef79a44d7ae1c4fa95e66a4897
Mut Ze <mutongsoft@gmail.com>
2019-06-10 17:23:49 +0800

    Main function's initial commit

---
+++ cmd/wxfetcher/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"os"
+
+	"bitbucket.org/mutongx/go-utils/log"
+	"google.golang.org/grpc"
+
+	"bitbucket.org/mutze5/wxfetcher/rpc"
+)
+
+func main() {
+
+	// Setup Logger Level
+	log.Level(log.Lnotice)
+
+	// Parse flags
+	rpcListen := flag.String("rpc-listen", ":9967", "Listen address and port for RPC server")
+	webListen := flag.String("web-listen", ":9968", "Listen address and port for web server")
+	flag.Parse()
+
+	// Start RPC Server
+	log.Notice("Main", "Starting WxFetcher RPC server at %s...", *rpcListen)
+	if rpcSocket, err := net.Listen("tcp", *rpcListen); err == nil {
+		rpcServer := grpc.NewServer()
+		rpc.RegisterWxFetcherServer(rpcServer, rpc.NewServer())
+		go rpcServer.Serve(rpcSocket)
+	} else {
+		log.Critical("Main", "Error creating RPC socket: %v", err)
+		os.Exit(1)
+	}
+
+	// Start the Web Server
+	log.Notice("Main", "Starting WxFetcher web server at %s...", *webListen)
+
+	select {}
+
+}
